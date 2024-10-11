af32a764e23f23722a12e231e62c3e5fa88c0fec
Sam <samuelkebbell@gmail.com>
2024-10-11 18:22:12 +0100

    Updates

--- src/make-flash10.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/Applications/flexsdk/bin/mxmlc -target-player=10.0.0 -debug=true -static-link-runtime-shared-libraries=true -optimize=true -o ../swf/soundmanager2_flash10_debug.swf -file-specs SoundManager2_AS3_Flash10.as
